2728c5c38220a8c6f96f161c4d1a2420f05433bc
DAAJ1999 <mail4dhruvpatel26@gmail.com>
2021-03-30 14:19:11 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,388 @@
+/**
+ * Here we have approximately 29 functions
+ * Each function represents a certain Key 
+ * Order of keys: TopKeys, MiddleKeys and BottomKeys 
+ * 
+ * The function begins by declaring a variable called "letter"
+ * getElementById is an in-built function in JS
+ * .innerHTML is used to output the appropriate value. Without .innerHTML, you cannot output it
+ *  
+ * For Example: 
+ * 
+ * There exist an ID of "text" in the HTML file. Using the .getElementById, we are able to access the FIELD and 
+ * with the help of .innerHTMl we are able to output it.
+ * 
+ * Once we have access to the correct ID, we manually assign that key to it's appropriate value
+ * In our case, the letter "Q"
+ *
+ *
+ * The function begins by declaring a variable called "img"
+ * getElementById is an in-built function in JS
+ * src.appenChild is used to get the image output
+ *
+ *
+ *
+ *
+ *
+ */
+
+function funQ() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'Q';
+	
+	
+}
+function funQ1(){
+var img = document.createElement("img");
+	img.src = "img/Q.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="70".height="70";
+}
+function funW() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'W';
+}
+function funW1(){
+var img = document.createElement("img");
+	img.src = "img/W.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="50".height="50";
+}
+
+function funE() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'E';
+}
+function funE1(){
+var img = document.createElement("img");
+	img.src = "img/E.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="50".height="50";
+}
+
+function funR() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'R';
+}
+function funR1(){
+var img = document.createElement("img");
+	img.src = "img/R.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="50".height="50";
+}
+
+function funT() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'T';
+}
+function funT1(){
+var img = document.createElement("img");
+	img.src = "img/T.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="50".height="50";
+}
+
+function funY() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'Y';
+}
+function funY1(){
+var img = document.createElement("img");
+	img.src = "img/Y.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="90".height="90";
+}
+
+function funU() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'U';
+}
+function funU1(){
+var img = document.createElement("img");
+	img.src = "img/U.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="90".height="90";
+}
+
+function funI() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'I';
+}
+function funI1(){
+var img = document.createElement("img");
+	img.src = "img/I.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="50".height="50";
+}
+
+function funO() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'O';
+}
+function funO1(){
+var img = document.createElement("img");
+	img.src = "img/O.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="50".height="50";
+}
+function funP() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'P';
+}
+function funP1(){
+var img = document.createElement("img");
+	img.src = "img/P.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="70".height="70";
+}
+
+function funA() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'A';
+}
+function funA1(){
+var img = document.createElement("img");
+	img.src = "img/A.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="90".height="90";
+}
+function funS() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'S';
+}
+function funS1(){
+var img = document.createElement("img");
+	img.src = "img/S.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="50".height="50";
+}
+
+function funD() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'D';
+}
+function funD1(){
+var img = document.createElement("img");
+	img.src = "img/D.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="100".height="100";
+}
+
+function funF() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'F';
+}
+function funF1(){
+var img = document.createElement("img");
+	img.src = "img/F.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="50".height="50";
+}
+
+function funG() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'G';
+}
+function funG1(){
+var img = document.createElement("img");
+	img.src = "img/G.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="100".height="100";
+}
+
+function funH() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'H';
+}
+function funH1(){
+var img = document.createElement("img");
+	img.src = "img/H.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="100".height="100";
+}
+
+function funJ() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'J';
+}
+function funJ1(){
+var img = document.createElement("img");
+	img.src = "img/J.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="90".height="90";
+}
+
+function funK() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'K';
+}
+function funK1(){
+var img = document.createElement("img");
+	img.src = "img/K.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="50".height="50";
+}
+function funL() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'L';
+}
+function funL1(){
+var img = document.createElement("img");
+	img.src = "img/L.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="70".height="70";
+}
+
+function funZ() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'Z';
+}
+function funZ1(){
+var img = document.createElement("img");
+	img.src = "img/Z.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="75".height="75";
+}
+function funX() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'X';
+}
+function funX1(){
+var img = document.createElement("img");
+	img.src = "img/X.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="100".height="100";
+}
+
+function funC() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'C';
+}
+function funC1(){
+var img = document.createElement("img");
+	img.src = "img/C.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="100".height="100";
+}
+
+function funV() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'V';
+}
+function funV1(){
+var img = document.createElement("img");
+	img.src = "img/V.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="100".height="100";
+}
+
+function funB() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'B';
+}
+function funB1(){
+var img = document.createElement("img");
+	img.src = "img/B.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="100".height="100";
+}
+
+function funN() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'N';
+}
+function funN1(){
+var img = document.createElement("img");
+	img.src = "img/N.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="70".height="70";
+}
+
+function funM() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'M';
+}
+function funM1(){
+var img = document.createElement("img");
+	img.src = "img/M.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="70".height="70";
+}
+function funQuestion() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'?';
+}
+function funQuestion1(){
+var img = document.createElement("img");
+	img.src = "images/Question.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="50".height="50";
+}
+function funPeriod() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML=letter+'.';
+
+}
+function funPeriod1(){
+var img = document.createElement("img");
+	img.src = "images/Period.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="50".height="50";
+}
+
+
+/**
+ * BackSpace function is similar to the above ones.
+ * First, we are getting the overall length of the text that was supplied and storing it in variable called temA
+ * Secondly, since backspace removes 1 letter each time, we set temB = 1
+ * We then, returned the lenght and stored it in variable called temC
+ * Once we have got the lenght, it was time to output 
+ * .slice returns or displays everything that's between the array index of 0 to the value of temC
+ */
+
+/**
+ * For example, let's say the text entered was "Aakash" and wanted to apply BackSpace
+ * 1) temA's length = 6
+ * 2) temB = 1
+ * 3) temC = temA - temB = 6 - 1 == 5
+ * 4) .slice(0, temC) => .slice(0,5) = "Aakash"
+ */
+
+function funBackSpace(){
+	var letter = document.getElementById('text').innerHTML;
+	var temA = document.getElementById('text').innerHTML.length;
+	var img= document.getElementById("Image");
+	var temB = 1;
+	var temC = temA - temB;
+	document.getElementById('text').innerHTML=letter.slice(0,temC);
+	img.removeChild(img.childNodes[temC])
+	
+} 
+
+/**
+ * Since we are clearing everything, we are supplying an empty string
+ * Empty String = no values. Everything resets
+ */
+
+function funClear(){
+	var letter = document.getElementById('text').innerHTML='';
+	var img=document.getElementById("Image");
+	while(img.firstChild){
+		img.removeChild(img.firstChild);
+	}
+	
+}
+
+/**
+ * This is simlar to the clear function BUT we are concatenating it with the previous letter that was passed
+ */
+function funSpace() {
+	var letter = document.getElementById('text').innerHTML;
+	document.getElementById('text').innerHTML= letter +' ';
+}
+function funSpace1(){
+var img = document.createElement("img");
+	img.src = "images/Space.png";
+	var src = document.getElementById("Image");
+	src.appendChild(img).width="50".height="50";
+}
